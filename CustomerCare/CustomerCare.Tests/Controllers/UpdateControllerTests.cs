27718aadaa8c33cdea1abb93085729c74319d9d6
TRUECLARITY\dwight pampellone <dwight.pampellone@trueclarity.co.uk>
2018-01-19 20:16:56 +0000

    Initial investment

---
+++ CustomerCare/CustomerCare.Tests/Controllers/UpdateControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace CustomerCare.Tests.Controllers
+{
+	[TestFixture]
+	public class UpdateControllerTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void CustomerProfileIaReturnedCorrectly()
+		{
+			Assert.That(1 == 1);
+		}
+	}
+}
---
+++ CustomerCare/CustomerCare/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomerCare.MvcApplication" Language="C#" %>
